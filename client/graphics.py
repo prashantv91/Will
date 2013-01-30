9c0e5bf84ca4fb27e2b2c9ce0ccc7140c1dba910
Prashant Vasudevan <prashantv91@gmail.com>
2013-01-30 12:29:51 +0530

    Added graphics config file. Changed model a bit.

--- client/graphics.py
+++ client/graphics.py
@@ -4,48 +4,91 @@ import curses
 from base import *
 
 colour_pair_map = {}        # Maps pairs of colours to colour pair numbers, as initialised by init_colours().
+creature_sprites = {}       # Maps from object name to colour pairs. These three filled in from config file by load_graphics_file().
+item_sprites = {}
+terrain_sprites = {}
+
+colours = {'black':curses.COLOR_BLACK, 'blue':curses.COLOR_BLUE, 'cyan':curses.COLOR_CYAN, 'green':curses.COLOR_GREEN, \
+        'magenta':curses.COLOR_MAGENTA, 'red':curses.COLOR_RED, 'white':curses.COLOR_WHITE, 'yellow':curses.COLOR_YELLOW, 'background': -1}
 
 class Sprite():
     # Store display information for an entity.
-    def __init__(self, _char = '@', _colours = (7, 0)):
-        if colour_pair_map == {}:
-            init_colours()
+    def init(self, char = '.', colour_pair = 0):
+        self.char = ord(char)      # Note that the @char that is given is to be a character.
+        self.colour_pair = colour_pair
 
-        self.char = ord(_char)      # Note that the @_char that is given is to be a character.
-        self.colour_pair = _colours
-
-class Screen(object):
-    # Has various windows that display stuff. 
+class Map_screen(object):
+    # To draw the map. Only the map - other windows are other classes. 
     __instance = None
     __instantiated = False
     
     def __new__(cls, *args, **kwargs):
         # To make sure only one instance of this class exists.
         if not cls.__instance:
-            cls.__instance = super(Screen, cls).__new__(cls, *args, **kwargs)
+            cls.__instance = super(Map_screen, cls).__new__(cls, *args, **kwargs)
         return cls.__instance
 
     def __init__(self, stdscr):
         if not self.__instantiated:
             self.map_pad = None
-            self.main_screen = curses.newwin(stdscr.getmayx()[0], stdscr.getmayx()[1], 0, 0)
+            self.map_screen_yx = stdscr.getmayx()          # This is what will change when more windows come in.
             self.__instantiated = True
-
-            curses.curs_set(0)
-            if colour_pair_map == {}:
-                init_colours()
         
-    def init_map_pad(self, height, width):
-        # Initialises map_pad.
-        self.map_pad = curses.newpad(height, width)
+    def init_map_pad(self, pad):
+        # Initialises map_pad to pad.
+        self.map_pad = pad
 
     def set_map_pad(self, pos, sprite):
+        # Currently not used. Use Map's set_map_pad() instead.
         self.map_pad.addch(pos.y, pos.x, sprite.char, sprite.colour_pair)
 
     def refresh(self):
         pass
 
+    def draw(self):
+        pass
+
+def init_graphics(graphics_config_file):
+    curses.curs_set(0)
+    init_colours()
+    load_graphics_file(graphics_config_file)
+
 
 def init_colours():
     # Initialise colour pairs and fill in some global dict.
-    pass
+    for i in range(8):
+        for j in range(-1,8):
+            curses.init_pair((i*9 + j + 1), i, j)
+            colour_pair_map[(i,j)] = curses.colour_pair((i*9+j+1))
+
+def load_graphics_file(filename):
+    # Loads graphics config info from the file @filename.
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+
+    for line in lines:
+        line = line.split('#')[0]
+        line = [part.strip() for part in line.split(':')]
+
+        if len(line) not in [4,5]:
+            continue
+        
+        if len(line) == 4:
+            line.append('background')
+
+        if line[3] not in colours or line[4] not in colours:
+            continue
+
+        name = line[1]
+        char = line[2][0]
+        colour_pair = colour_pair_map[(colours[line[3]], colours[line[4]])]
+        sprite = Sprite(char, colour_pair)
+        
+        if line[0] == 'terrain':
+            terrain_sprites[name] = sprite
+        elif line[0] == 'creature':
+            creature_sprites[name] = sprite
+        elif line[0] == 'item':
+            item_sprites[name] = sprite
+
--- client/map.py
+++ client/map.py
@@ -2,34 +2,51 @@
 
 from graphics import *
 
-class Object():
-    # Stores the type of object, it's sprite and an annotation.
-    def __init__(self, _type = None, _sprite = Sprite(), _annotation = ''):
-        self.type = _type
-        self.sprite = _sprite
-        self.annotation = _annotation
+class Creature():
+    # Stores the type of craeture, it's sprite and an annotation.
+    def init(self, name = '', annotation = ''):
+        self.name = name
+        self.sprite = creature_sprites[name]
+        self.annotation = annotation
+
+class Item():
+    # Stores the type of item, it's sprite and an annotation.
+    def init(self, name = '', annotation = ''):
+        self.name = name
+        self.sprite = item_sprites[name]
+        self.annotation = annotation
 
 class Terrain():
     # To store info about a particular terrain. Might be more useful later.
-    def __init__(self, _sprite = Sprite()):
-        self.sprite = _sprite
+    def init(self, name):
+        self.name = name
+        self.sprite = terrain_sprites[name]
 
 class Tile():
     # Info for a map tile.
-    def __init__(self, _terrain = None, _creature = None, _items = []):
-        self.terrain = _terrain
-        self.creature = _creature
-        self.items = _items
+    def init(self, terrain = None, creature = None, items = [], visible = False):
+        self.terrain = terrain
+        self.creature = creature
+        self.items = items
+        self.visible = visible
 
 class Map():
     # A rectangular collection of tiles.
-    def __init__(self, _height, _width, _screen):
-        self.height = _height
-        self.width = _width
+    def init(self, name, height, width, screen):
+        self.name = name
+        self.height = height
+        self.width = width
         self.map = [[Tile() for i in range(self.width)] for j in range(self.height)]
-        self.screen = _screen
+        self.screen = screen
+        self.map_pad = curses.newpad(self.height, self.width)
 
-    def update_tile(self, pos, terrain = -1, creature = -1, items = -1):
+    def set_map_pad(self, pos, sprite, bold = False):
+        if bold:
+            self.map_pad.addch(pos.y, pos.x, sprite.char, sprite.colour_pair | curses.A_BOLD)
+        else:
+            self.map_pad.addch(pos.y, pos.x, sprite.char, sprite.colour_pair)
+
+    def update_tile(self, pos, terrain = -1, creature = -1, items = -1, visible = False):
         # Updates tile at (pos.y, pos.x). Couldn't use None as invalid indicator.
         if terrain != -1:
             self.map[pos.y][pos.x].terrain = terrain
@@ -37,31 +54,28 @@ class Map():
             self.map[pos.y][pos.x].items = items
         if creature != -1:
             self.map[pos.y][pos.x].creature = creature
+        self.map[pos.y][pos.x].visible = visible
         
         if self.map[pos.y][pos.x].items == [] and self.map[pos.y][pos.x].creature == None:
-            self.screen.set_map_pad(pos, self.map[pos.y][pos.x].terrain.sprite)
+            self.set_map_pad(pos, self.map[pos.y][pos.x].terrain.sprite, visible)
         elif self.map[pos.y][pos.x].items != [] and self.map[pos.y][pos.x].creature == None:
-            self.screen.set_map_pad(pos, self.map[pos.y][pos.x].items[0].sprite)
+            self.set_map_pad(pos, self.map[pos.y][pos.x].items[0].sprite)
         else:
-            self.screen.set_map_pad(pos, self.map[pos.y][pos.x].creature.sprite)
+            self.set_map_pad(pos, self.map[pos.y][pos.x].creature.sprite)
 
     def set_tile(self, pos, terrain, creature = None, items = []):
-        # Sets tile at (pos.y, pos.x). 
-        # Note that this doesn't call the screen's update function. This is to save on locking time.
+        # Sets tile at (pos.y, pos.x) and updates this instance's map_pad.
         self.map[pos.y][pos.x] = Tile(terrain, creature, items)
+        if self.map[pos.y][pos.x].items == [] and self.map[pos.y][pos.x].creature == None:
+            self.set_map_pad(pos, self.map[pos.y][pos.x].terrain.sprite)
+        elif self.map[pos.y][pos.x].items != [] and self.map[pos.y][pos.x].creature == None:
+            self.set_map_pad(pos, self.map[pos.y][pos.x].items[0].sprite)
+        else:
+            self.set_map_pad(pos, self.map[pos.y][pos.x].creature.sprite)
 
-    def update_screen(self):
-        # Synchronises screen.map_pad with this map. To be called immediately after many calls to set_tile.
-        self.screen.init_map_pad(self.height, self.width)
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.map[pos.y][pos.x].items == [] and self.map[pos.y][pos.x].creature == None:
-                    self.screen.set_map_pad(pos, self.map[pos.y][pos.x].terrain.sprite)
-                elif self.map[pos.y][pos.x].items != [] and self.map[pos.y][pos.x].creature == None:
-                    self.screen.set_map_pad(pos, self.map[pos.y][pos.x].items[0].sprite)
-                else:
-                    self.screen.set_map_pad(pos, self.map[pos.y][pos.x].creature.sprite)
-
+    def sync_screen(self):
+        # Synchronises screen.map_pad with this map.
+        self.screen.init_map_pad(self.map_pad)
         
 
     
